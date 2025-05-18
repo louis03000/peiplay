80eedb8209f2d690c25d2bdb69f5aabce2c816f2
louis03000 <louis92427@gmail.com>
2025-05-19 02:14:24 +0800

    fix: login 頁面 revalidate=0，徹底解決 useSession 預渲染 build fail

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 export const dynamic = 'force-dynamic'
+export const revalidate = 0
 
 import { signIn, useSession } from 'next-auth/react'
 import LineLoginButton from '@/components/LineLoginButton'
