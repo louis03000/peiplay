2ba1c7b1d3323e1bba4068f4038c01c2ce4e658b
louis03000 <louis92427@gmail.com>
2025-05-22 17:53:16 +0800

    fix: bookings 查詢頁面 dynamic import 並禁止 SSR，徹底解決 useSession 預渲染錯誤

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -1,4 +1,6 @@
-import BookingsClient from "./BookingsClient";
+import dynamic from "next/dynamic";
+const BookingsClient = dynamic(() => import("./BookingsClient"), { ssr: false });
+
 export default function Page() {
   return <BookingsClient />;
 } 
