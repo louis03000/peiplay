5d47f2f4ab97e7e77f6bda70b668afcde11290b4
louis03000 <louis92427@gmail.com>
2025-05-30 04:39:44 +0800

    test: layout 加回 Navbar 和 Footer，逐步排查 crash 問題

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,9 @@ export default function RootLayout({
     <html lang="zh-TW">
       <body>
         <Providers>
+          <ClientNavbar />
           {children}
+          <Footer />
         </Providers>
       </body>
     </html>
